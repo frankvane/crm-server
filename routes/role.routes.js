9cd09ff1d986209c29511243e750fd4263beb5bf
vane <13525442@qq.com>
2025-04-29 22:36:21 +0800

    fix: 修改角色路由中的中间件使用方式

--- routes/role.routes.js
+++ routes/role.routes.js
@@ -1,45 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const roleController = require("../controllers/role.controller");
-const { authenticateToken, hasPermission } = require("../middlewares/auth");
+const { verifyToken, hasPermission } = require("../middlewares/auth");
 
 // 创建角色
 router.post(
   "/",
-  authenticateToken,
-  hasPermission("manage_roles"),
+  [verifyToken, hasPermission("manage_roles")],
   roleController.createRole
 );
 
 // 获取角色列表
 router.get(
   "/",
-  authenticateToken,
-  hasPermission("view_roles"),
+  [verifyToken, hasPermission("view_roles")],
   roleController.getRoles
 );
 
 // 获取单个角色
 router.get(
   "/:id",
-  authenticateToken,
-  hasPermission("view_roles"),
+  [verifyToken, hasPermission("view_roles")],
   roleController.getRole
 );
 
 // 更新角色
 router.put(
   "/:id",
-  authenticateToken,
-  hasPermission("manage_roles"),
+  [verifyToken, hasPermission("manage_roles")],
   roleController.updateRole
 );
 
 // 删除角色
 router.delete(
   "/:id",
-  authenticateToken,
-  hasPermission("manage_roles"),
+  [verifyToken, hasPermission("manage_roles")],
   roleController.deleteRole
 );
 
