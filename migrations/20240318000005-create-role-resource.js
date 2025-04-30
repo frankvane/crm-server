bd28b751aafe343cad3032ad56cf59c7a6a264bf
vane <13525442@qq.com>
2025-04-30 21:11:45 +0800

    feat: 添加角色资源关联表迁移文件

---
+++ migrations/20240318000005-create-role-resource.js
@@ -0,0 +1,47 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("RoleResources", {
+      roleId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Roles",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      resourceId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Resources",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    // Add a composite primary key
+    await queryInterface.addConstraint("RoleResources", {
+      fields: ["roleId", "resourceId"],
+      type: "primary key",
+      name: "role_resource_pkey",
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("RoleResources");
+  },
+};
