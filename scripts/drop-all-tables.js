3c7f429db5d17c5b92811c6a084abc1cc5d85e70
vane <13525442@qq.com>
2025-05-01 09:12:37 +0800

    fix: 修复数据库初始化时的扩展属性问题，完善用户角色权限设置

--- scripts/drop-all-tables.js
+++ scripts/drop-all-tables.js
@@ -26,6 +26,24 @@ async function dropAllTables() {
       AND TABLE_SCHEMA = 'dbo'
     `);
 
+    // 删除所有扩展属性
+    await sequelize.query(`
+      DECLARE @SQL NVARCHAR(MAX) = N'';
+      SELECT @SQL += N'
+      EXEC sys.sp_dropextendedproperty
+        @name = N''' + ep.name + N''',
+        @level0type = N''SCHEMA'',
+        @level0name = N''' + OBJECT_SCHEMA_NAME(ep.major_id) + N''',
+        @level1type = N''TABLE'',
+        @level1name = N''' + OBJECT_NAME(ep.major_id) + N''',
+        @level2type = N''COLUMN'',
+        @level2name = N''' + c.name + N''';'
+      FROM sys.extended_properties ep
+      JOIN sys.columns c ON ep.major_id = c.object_id AND ep.minor_id = c.column_id
+      WHERE ep.class = 1;
+      EXEC sp_executesql @SQL;
+    `);
+
     // 禁用外键约束
     await sequelize.query(
       'EXEC sp_MSforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"'
@@ -38,7 +56,9 @@ async function dropAllTables() {
       console.log(`Dropped table: ${tableName}`);
     }
 
-    console.log("All tables have been dropped successfully");
+    console.log(
+      "All tables and extended properties have been dropped successfully"
+    );
   } catch (error) {
     console.error("Error dropping tables:", error);
   } finally {
