c5fb0a77108b2e065ab744124840a1a292a77ecc
vane <13525442@qq.com>
2025-05-07 15:38:37 +0800

    chore: 注册大文件分片相关路由，确保接口可用

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ router.use("/categories", require("./category.routes"));
 router.use("/category-types", require("./categoryType.routes"));
 router.use("/roles", require("./role.routes"));
 router.use("/resources", require("./resource.routes"));
+router.use(require("./file.routes"));
 
 module.exports = router;
